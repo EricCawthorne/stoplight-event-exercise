c1aca90b46683041a502615fd9f1cc57dcdf54b3
EricCawthorne <cawthorneeric@gmail.com>
2021-09-03 15:29:47 -0600

    completed

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
 (function() {
-  'use strict';
+  //get button IDs
+  let stopButton = document.getElementById('stopButton')
+  let slowButton = document.getElementById('slowButton')
+  let goButton = document.getElementById('goButton')
+  //get light IDs
+  let redLight = document.getElementById('stopLight')
+  let slowLight = document.getElementById('slowLight')
+  let goLight = document.getElementById('goLight')
+  //assign buttons to toggle lights
+  stopButton.addEventListener('click', function() {
+    redLight.classList.toggle('stop')
+  });
+ 
+  slowButton.addEventListener('click', function() {
+    slowLight.classList.toggle('slow') 
+  });
 
-  // YOUR CODE HERE
-})();
+  goButton.addEventListener('click', function() {
+    goLight.classList.toggle('go')
+  });
+  //Part 2
+  stopButton.addEventListener('mouseenter', function() {
+    console.log('Entered Stop Button')
+  });
+
+  stopButton.addEventListener('mouseleave', function() {
+    console.log('Left Stop Button')
+  });
+
+  slowButton.addEventListener('mouseenter', function() {
+    console.log('Entered Slow Button')
+  });
+
+  slowButton.addEventListener('mouseleave', function() {
+    console.log('Left Slow Button')
+  });
+
+  goButton.addEventListener('mouseenter', function() {
+    console.log('Entered Go Button')
+  });
+
+  goButton.addEventListener('mouseleave', function() {
+    console.log('Left Go Button')
+  });
+  //bonus
+  let controls = document.getElementById('#controls')
+}) ();
